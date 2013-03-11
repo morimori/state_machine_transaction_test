7d9af48de0b16fad61eb38678874e737db57d342
Takatoshi MORIYAMA <hawk@at-exit.com>
2013-03-11 12:31:12 +0900

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+StateMachineTransactionTest::Application.config.secret_token = '757e6779cfde22e18a7dea3dbc56c40d217432ece6a9fbdac5058cc038d9a2b94aa83aaa7699aaf623d40a5efb84620e4557db7a5768278f6071329cf664c560'
